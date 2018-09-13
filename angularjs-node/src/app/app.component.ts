ca869f04c985a81b17d024ffac9e2bfaf35c8d37
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2018-09-13 14:34:45 +0200

    Call the service in the page initialization

--- angularjs-node/src/app/app.component.ts
+++ angularjs-node/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
 export class AppComponent {
   title = 'angularjs-node';
+
+  data: any = []
+  
+  constructor(public appService: AppService) { }
+
+  ngOnInit() {
+    this.UsersPage();
+  }
+
+  /* method to call get-api from app.service */
+  UsersPage() {
+    try {
+      this.appService.getUsers()
+        .subscribe(resp => {
+          console.log(resp, "res");
+          this.data = resp
+        },
+          error => {
+            console.log(error, "error");
+          })
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
