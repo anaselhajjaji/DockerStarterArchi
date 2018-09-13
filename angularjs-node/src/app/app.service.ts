9846608ab7124b18ad69acfa81d1ff1f85781b30
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2018-09-13 14:33:57 +0200

    Add the call to the rest api in the service

--- angularjs-node/src/app/app.service.ts
+++ angularjs-node/src/app/app.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
 export class AppService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  /* Uses http.get() to load data from a single API endpoint */
+  getUsers() {
+    return this.http.get('https://api.github.com/users');
+  }
 }
