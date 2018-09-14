6faf15116737a2a31ab036ef9d686371d67190cd
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2018-09-14 11:22:53 +0200

    to authorize connections

--- nodejs-node/server.js
+++ nodejs-node/server.js
@@ -24,6 +24,26 @@ var config = {
 var pool = new pg.Pool(config)
 var myClient
 
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.post('/api/insert', function(req, res) {
   var username = req.body.uname;
   
