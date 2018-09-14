8d8e87bc1249d3e26472e106fc20c5139807f13b
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2018-09-14 10:59:00 +0200

    connect angular app to nodejs-node

--- angularjs-node/src/app/app.service.ts
+++ angularjs-node/src/app/app.service.ts
@@ -11,11 +11,11 @@ export class AppService {
   constructor(private http: HttpClient) { }
   /* Uses http.get() to load data from a single API endpoint */
   getUsers() {
-    return this.http.get('https://api.github.com/users');
+    return this.http.get('https://172.18.0.3:8080/api/list');
   }
 
   /* Uses http.post() to submit data from a single API endpoint */
   submitUser(data) {
-    return this.http.post('http://httpbin.org/post', data);
+    return this.http.post('https://172.18.0.3:8080/api/insert', data);
   }
 }
